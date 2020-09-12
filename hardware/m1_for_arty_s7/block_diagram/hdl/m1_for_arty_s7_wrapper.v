fd5dec1fd4e992f577b9229e102b85f7fd9581c3
Jean Cyr <jean.m.cyr@gmail.com>
2020-09-11 21:44:01 -0400

    1st build

--- hardware/m1_for_arty_s7/block_diagram/hdl/m1_for_arty_s7_wrapper.v
+++ hardware/m1_for_arty_s7/block_diagram/hdl/m1_for_arty_s7_wrapper.v
@@ -1,8 +1,8 @@
 //Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2018.2 (win64) Build 2258646 Thu Jun 14 20:03:12 MDT 2018
-//Date        : Thu Dec 13 14:36:41 2018
-//Host        : E110832 running 64-bit major release  (build 9200)
+//Date        : Fri Sep 11 21:19:23 2020
+//Host        : QOSMIO running 64-bit major release  (build 9200)
 //Command     : generate_target m1_for_arty_s7_wrapper.bd
 //Design      : m1_for_arty_s7_wrapper
 //Purpose     : IP block netlist
@@ -14,7 +14,7 @@ module m1_for_arty_s7_wrapper
     TDI,
     TDO,
     dip_switches_4bits_tri_i,
-    led_4bits_tri_io,
+    led_4bits_tri_o,
     nTRST,
     push_buttons_4bits_tri_i,
     qspi_flash_io0_io,
@@ -23,7 +23,7 @@ module m1_for_arty_s7_wrapper
     qspi_flash_io3_io,
     qspi_flash_ss_io,
     reset,
-    rgb_led_tri_io,
+    rgb_led_tri_o,
     sys_clock,
     usb_uart_rxd,
     usb_uart_txd);
@@ -31,7 +31,7 @@ module m1_for_arty_s7_wrapper
   input TDI;
   output [0:0]TDO;
   input [3:0]dip_switches_4bits_tri_i;
-  inout [3:0]led_4bits_tri_io;
+  output [3:0]led_4bits_tri_o;
   input nTRST;
   input [3:0]push_buttons_4bits_tri_i;
   inout qspi_flash_io0_io;
@@ -40,7 +40,7 @@ module m1_for_arty_s7_wrapper
   inout qspi_flash_io3_io;
   inout qspi_flash_ss_io;
   input reset;
-  inout [5:0]rgb_led_tri_io;
+  output [5:0]rgb_led_tri_o;
   input sys_clock;
   input usb_uart_rxd;
   output usb_uart_txd;
@@ -49,22 +49,7 @@ module m1_for_arty_s7_wrapper
   wire TDI;
   wire [0:0]TDO;
   wire [3:0]dip_switches_4bits_tri_i;
-  wire [0:0]led_4bits_tri_i_0;
-  wire [1:1]led_4bits_tri_i_1;
-  wire [2:2]led_4bits_tri_i_2;
-  wire [3:3]led_4bits_tri_i_3;
-  wire [0:0]led_4bits_tri_io_0;
-  wire [1:1]led_4bits_tri_io_1;
-  wire [2:2]led_4bits_tri_io_2;
-  wire [3:3]led_4bits_tri_io_3;
-  wire [0:0]led_4bits_tri_o_0;
-  wire [1:1]led_4bits_tri_o_1;
-  wire [2:2]led_4bits_tri_o_2;
-  wire [3:3]led_4bits_tri_o_3;
-  wire [0:0]led_4bits_tri_t_0;
-  wire [1:1]led_4bits_tri_t_1;
-  wire [2:2]led_4bits_tri_t_2;
-  wire [3:3]led_4bits_tri_t_3;
+  wire [3:0]led_4bits_tri_o;
   wire nTRST;
   wire [3:0]push_buttons_4bits_tri_i;
   wire qspi_flash_io0_i;
@@ -88,62 +73,17 @@ module m1_for_arty_s7_wrapper
   wire qspi_flash_ss_o;
   wire qspi_flash_ss_t;
   wire reset;
-  wire [0:0]rgb_led_tri_i_0;
-  wire [1:1]rgb_led_tri_i_1;
-  wire [2:2]rgb_led_tri_i_2;
-  wire [3:3]rgb_led_tri_i_3;
-  wire [4:4]rgb_led_tri_i_4;
-  wire [5:5]rgb_led_tri_i_5;
-  wire [0:0]rgb_led_tri_io_0;
-  wire [1:1]rgb_led_tri_io_1;
-  wire [2:2]rgb_led_tri_io_2;
-  wire [3:3]rgb_led_tri_io_3;
-  wire [4:4]rgb_led_tri_io_4;
-  wire [5:5]rgb_led_tri_io_5;
-  wire [0:0]rgb_led_tri_o_0;
-  wire [1:1]rgb_led_tri_o_1;
-  wire [2:2]rgb_led_tri_o_2;
-  wire [3:3]rgb_led_tri_o_3;
-  wire [4:4]rgb_led_tri_o_4;
-  wire [5:5]rgb_led_tri_o_5;
-  wire [0:0]rgb_led_tri_t_0;
-  wire [1:1]rgb_led_tri_t_1;
-  wire [2:2]rgb_led_tri_t_2;
-  wire [3:3]rgb_led_tri_t_3;
-  wire [4:4]rgb_led_tri_t_4;
-  wire [5:5]rgb_led_tri_t_5;
+  wire [5:0]rgb_led_tri_o;
   wire sys_clock;
   wire usb_uart_rxd;
   wire usb_uart_txd;
 
-  IOBUF led_4bits_tri_iobuf_0
-       (.I(led_4bits_tri_o_0),
-        .IO(led_4bits_tri_io[0]),
-        .O(led_4bits_tri_i_0),
-        .T(led_4bits_tri_t_0));
-  IOBUF led_4bits_tri_iobuf_1
-       (.I(led_4bits_tri_o_1),
-        .IO(led_4bits_tri_io[1]),
-        .O(led_4bits_tri_i_1),
-        .T(led_4bits_tri_t_1));
-  IOBUF led_4bits_tri_iobuf_2
-       (.I(led_4bits_tri_o_2),
-        .IO(led_4bits_tri_io[2]),
-        .O(led_4bits_tri_i_2),
-        .T(led_4bits_tri_t_2));
-  IOBUF led_4bits_tri_iobuf_3
-       (.I(led_4bits_tri_o_3),
-        .IO(led_4bits_tri_io[3]),
-        .O(led_4bits_tri_i_3),
-        .T(led_4bits_tri_t_3));
   m1_for_arty_s7 m1_for_arty_s7_i
        (.DAPLink_tri_o(DAPLink_tri_o),
         .TDI(TDI),
         .TDO(TDO),
         .dip_switches_4bits_tri_i(dip_switches_4bits_tri_i),
-        .led_4bits_tri_i({led_4bits_tri_i_3,led_4bits_tri_i_2,led_4bits_tri_i_1,led_4bits_tri_i_0}),
-        .led_4bits_tri_o({led_4bits_tri_o_3,led_4bits_tri_o_2,led_4bits_tri_o_1,led_4bits_tri_o_0}),
-        .led_4bits_tri_t({led_4bits_tri_t_3,led_4bits_tri_t_2,led_4bits_tri_t_1,led_4bits_tri_t_0}),
+        .led_4bits_tri_o(led_4bits_tri_o),
         .nTRST(nTRST),
         .push_buttons_4bits_tri_i(push_buttons_4bits_tri_i),
         .qspi_flash_io0_i(qspi_flash_io0_i),
@@ -162,9 +102,7 @@ module m1_for_arty_s7_wrapper
         .qspi_flash_ss_o(qspi_flash_ss_o),
         .qspi_flash_ss_t(qspi_flash_ss_t),
         .reset(reset),
-        .rgb_led_tri_i({rgb_led_tri_i_5,rgb_led_tri_i_4,rgb_led_tri_i_3,rgb_led_tri_i_2,rgb_led_tri_i_1,rgb_led_tri_i_0}),
-        .rgb_led_tri_o({rgb_led_tri_o_5,rgb_led_tri_o_4,rgb_led_tri_o_3,rgb_led_tri_o_2,rgb_led_tri_o_1,rgb_led_tri_o_0}),
-        .rgb_led_tri_t({rgb_led_tri_t_5,rgb_led_tri_t_4,rgb_led_tri_t_3,rgb_led_tri_t_2,rgb_led_tri_t_1,rgb_led_tri_t_0}),
+        .rgb_led_tri_o(rgb_led_tri_o),
         .sys_clock(sys_clock),
         .usb_uart_rxd(usb_uart_rxd),
         .usb_uart_txd(usb_uart_txd));
@@ -193,34 +131,4 @@ module m1_for_arty_s7_wrapper
         .IO(qspi_flash_ss_io),
         .O(qspi_flash_ss_i),
         .T(qspi_flash_ss_t));
-  IOBUF rgb_led_tri_iobuf_0
-       (.I(rgb_led_tri_o_0),
-        .IO(rgb_led_tri_io[0]),
-        .O(rgb_led_tri_i_0),
-        .T(rgb_led_tri_t_0));
-  IOBUF rgb_led_tri_iobuf_1
-       (.I(rgb_led_tri_o_1),
-        .IO(rgb_led_tri_io[1]),
-        .O(rgb_led_tri_i_1),
-        .T(rgb_led_tri_t_1));
-  IOBUF rgb_led_tri_iobuf_2
-       (.I(rgb_led_tri_o_2),
-        .IO(rgb_led_tri_io[2]),
-        .O(rgb_led_tri_i_2),
-        .T(rgb_led_tri_t_2));
-  IOBUF rgb_led_tri_iobuf_3
-       (.I(rgb_led_tri_o_3),
-        .IO(rgb_led_tri_io[3]),
-        .O(rgb_led_tri_i_3),
-        .T(rgb_led_tri_t_3));
-  IOBUF rgb_led_tri_iobuf_4
-       (.I(rgb_led_tri_o_4),
-        .IO(rgb_led_tri_io[4]),
-        .O(rgb_led_tri_i_4),
-        .T(rgb_led_tri_t_4));
-  IOBUF rgb_led_tri_iobuf_5
-       (.I(rgb_led_tri_o_5),
-        .IO(rgb_led_tri_io[5]),
-        .O(rgb_led_tri_i_5),
-        .T(rgb_led_tri_t_5));
 endmodule

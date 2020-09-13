3c53d929ca3c0b4510125f0afc32a4df57e36928
Jean M. Cyr <jean.m.cyr@gmail.com>
2020-09-13 04:56:50 -0400

    Fails timing

--- hardware/m1_for_arty_s7/block_diagram/hdl/m1_for_arty_s7_wrapper.v
+++ hardware/m1_for_arty_s7/block_diagram/hdl/m1_for_arty_s7_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
-//Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
-//Date        : Sun Sep 13 00:11:40 2020
+//Tool Version: Vivado v.2018.3 (lin64) Build 2405991 Thu Dec  6 23:36:41 MST 2018
+//Date        : Sun Sep 13 04:43:26 2020
 //Host        : compute running 64-bit Ubuntu 18.04.5 LTS
 //Command     : generate_target m1_for_arty_s7_wrapper.bd
 //Design      : m1_for_arty_s7_wrapper
@@ -10,7 +10,22 @@
 `timescale 1 ps / 1 ps
 
 module m1_for_arty_s7_wrapper
-   (dip_switches_4bits_tri_i,
+   (ddr3_sdram_addr,
+    ddr3_sdram_ba,
+    ddr3_sdram_cas_n,
+    ddr3_sdram_ck_n,
+    ddr3_sdram_ck_p,
+    ddr3_sdram_cke,
+    ddr3_sdram_cs_n,
+    ddr3_sdram_dm,
+    ddr3_sdram_dq,
+    ddr3_sdram_dqs_n,
+    ddr3_sdram_dqs_p,
+    ddr3_sdram_odt,
+    ddr3_sdram_ras_n,
+    ddr3_sdram_reset_n,
+    ddr3_sdram_we_n,
+    dip_switches_4bits_tri_i,
     led_4bits_tri_o,
     push_buttons_4bits_tri_i,
     qspi_flash_io0_io,
@@ -25,6 +40,21 @@ module m1_for_arty_s7_wrapper
     sys_clock,
     usb_uart_rxd,
     usb_uart_txd);
+  output [13:0]ddr3_sdram_addr;
+  output [2:0]ddr3_sdram_ba;
+  output ddr3_sdram_cas_n;
+  output [0:0]ddr3_sdram_ck_n;
+  output [0:0]ddr3_sdram_ck_p;
+  output [0:0]ddr3_sdram_cke;
+  output [0:0]ddr3_sdram_cs_n;
+  output [1:0]ddr3_sdram_dm;
+  inout [15:0]ddr3_sdram_dq;
+  inout [1:0]ddr3_sdram_dqs_n;
+  inout [1:0]ddr3_sdram_dqs_p;
+  output [0:0]ddr3_sdram_odt;
+  output ddr3_sdram_ras_n;
+  output ddr3_sdram_reset_n;
+  output ddr3_sdram_we_n;
   input [3:0]dip_switches_4bits_tri_i;
   output [3:0]led_4bits_tri_o;
   input [3:0]push_buttons_4bits_tri_i;
@@ -41,6 +71,21 @@ module m1_for_arty_s7_wrapper
   input usb_uart_rxd;
   output usb_uart_txd;
 
+  wire [13:0]ddr3_sdram_addr;
+  wire [2:0]ddr3_sdram_ba;
+  wire ddr3_sdram_cas_n;
+  wire [0:0]ddr3_sdram_ck_n;
+  wire [0:0]ddr3_sdram_ck_p;
+  wire [0:0]ddr3_sdram_cke;
+  wire [0:0]ddr3_sdram_cs_n;
+  wire [1:0]ddr3_sdram_dm;
+  wire [15:0]ddr3_sdram_dq;
+  wire [1:0]ddr3_sdram_dqs_n;
+  wire [1:0]ddr3_sdram_dqs_p;
+  wire [0:0]ddr3_sdram_odt;
+  wire ddr3_sdram_ras_n;
+  wire ddr3_sdram_reset_n;
+  wire ddr3_sdram_we_n;
   wire [3:0]dip_switches_4bits_tri_i;
   wire [3:0]led_4bits_tri_o;
   wire [3:0]push_buttons_4bits_tri_i;
@@ -73,7 +118,22 @@ module m1_for_arty_s7_wrapper
   wire usb_uart_txd;
 
   m1_for_arty_s7 m1_for_arty_s7_i
-       (.dip_switches_4bits_tri_i(dip_switches_4bits_tri_i),
+       (.ddr3_sdram_addr(ddr3_sdram_addr),
+        .ddr3_sdram_ba(ddr3_sdram_ba),
+        .ddr3_sdram_cas_n(ddr3_sdram_cas_n),
+        .ddr3_sdram_ck_n(ddr3_sdram_ck_n),
+        .ddr3_sdram_ck_p(ddr3_sdram_ck_p),
+        .ddr3_sdram_cke(ddr3_sdram_cke),
+        .ddr3_sdram_cs_n(ddr3_sdram_cs_n),
+        .ddr3_sdram_dm(ddr3_sdram_dm),
+        .ddr3_sdram_dq(ddr3_sdram_dq),
+        .ddr3_sdram_dqs_n(ddr3_sdram_dqs_n),
+        .ddr3_sdram_dqs_p(ddr3_sdram_dqs_p),
+        .ddr3_sdram_odt(ddr3_sdram_odt),
+        .ddr3_sdram_ras_n(ddr3_sdram_ras_n),
+        .ddr3_sdram_reset_n(ddr3_sdram_reset_n),
+        .ddr3_sdram_we_n(ddr3_sdram_we_n),
+        .dip_switches_4bits_tri_i(dip_switches_4bits_tri_i),
         .led_4bits_tri_o(led_4bits_tri_o),
         .push_buttons_4bits_tri_i(push_buttons_4bits_tri_i),
         .qspi_flash_io0_i(qspi_flash_io0_i),
